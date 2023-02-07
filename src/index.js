5dc07f11cb39f718952b64cde97052da72c15c24
abeauvez <arnaud.beauvez@nordnet.com>
2023-02-07 21:51:55 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,71 +1,77 @@
 import "./style.css";
 
-const ul = document.querySelector('ul');
-const form = document.querySelector('form');
-const input = document.querySelector('form > input');
+document.addEventListener("DOMContentLoaded", function () {
+    const ul = document.querySelector('ul');
+    const form = document.querySelector("form");
+    const input = document.querySelector('form > input');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const value = input.value;
-    input.value = '';
-    addTodo(value);
-});
+    const todos = [
+        {
+            text: 'je suis une todo',
+            done: false
+        },
+        {
+            text: 'faire du JavaScript',
+            done: true
+        },
+    ];
 
-const todos = [
-    {
-        text: 'je suis une todo',
-        done: false
-    },
-    {
-        text: 'faire du JavaScript',
-        done: true
-    },
-];
+    const displayTodo = () => {
+        ul.innerHTML = "";
+        const todosNode = todos.map((todo, index) => {
+            return createTodoElement(todo, index);
+        })
+        ul.append(...todosNode);
+    };
 
-const displayTodo = () => {
-    const todosNode = todos.map((todo, index) => {
-        return createTodoElement(todo, index);
-    })
-    ul.innerHTML = '';
-    ul.append(...todosNode);
-};
+    const createTodoElement = (todo, index) => {
+        const li = document.createElement('li');
+        const buttonDelete = document.createElement('button');
+        buttonDelete.innerHTML = 'Supprimer';
+        buttonDelete.addEventListener('click', (event) => {
+            event.stopPropagation();
+            deleteTodo(index);
+        });
 
-const createTodoElement = (todo, index) => {
-    const li = document.createElement('li');
-    const buttonDelete = document.createElement('button');
-    buttonDelete.innerHTML = 'Supprimer';
-    buttonDelete.addEventListener('click', (event) => {
-        event.stopPropagation();
-        deleteTodo(index);
-    });
-
-    li.innerHTML = `
+        li.innerHTML = `
         <span class = "todo ${todo.done ? 'done' : ''}"></span>
         <p>${todo.text}</p>
         <button>Supprimer</button> 
     `;
-    li.addEventListener('click', (event) => {
-        toggleTodo(index);
+        li.addEventListener('click', (event) => {
+            toggleTodo(index);
+        });
+        li.appendChild(buttonDelete)
+        return li;
+    };
+
+    const addTodo = (text) => {
+        todos.push({
+            text,
+            done: false
+        });
+        displayTodo();
+    };
+
+    const deleteTodo = (index) => {
+        todos.splice(index, 1);
+        displayTodo();
+    }
+
+    const toggleTodo = index => {
+        todos[index].done = !todos[index].done;
+        displayTodo();
+    }
+
+    displayTodo();
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = input.value;
+        input.value = '';
+        addTodo(value);
     });
-    li.appendChild(buttonDelete)
-    return li;
-};
 
-const addTodo = (text) => {
-    todos.push({
-        text,
-        done: false
-    });
-    displayTodo();
-};
+});
 
-const deleteTodo = (index) => {
-    todos.splice(index, 1);
-    displayTodo();
-}
 
-const toggleTodo = index => {
-    todos[index].done = !todos[index].done;
-    displayTodo();
-}
-displayTodo();
